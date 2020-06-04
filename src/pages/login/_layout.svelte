628880e763da48bf95491b3ed14df516bf1b2edb
silvestrevivo <silvestrevivomillan@gmail.com>
2020-06-05 00:38:49 +0200

    start point for user registration

---
+++ src/pages/login/_layout.svelte
@@ -0,0 +1,28 @@
+<script>
+    import { url, isActive } from '@sveltech/routify';
+    const links = [
+        ['./', 'login'],
+        ['./signup', 'sign up'],
+        ['./forgotpassword', 'forgot password'],
+    ];
+</script>
+
+<div class="debug">
+    <ul>
+        {#each links as [path, name] (path)}
+            <a
+                href={$url(path)}
+                class="rounded-l-lg block h-12 p-2 inline-block no-underline"
+                class:active={$isActive(path)}>
+                <span>{name}</span>
+            </a>
+        {/each}
+    </ul>
+    <slot />
+</div>
+
+<style>
+    a.active {
+        color: red;
+    }
+</style>
---
+++ src/pages/login/forgotpassword.svelte
@@ -0,0 +1,1 @@
+<p>forgotpassword page</p>
